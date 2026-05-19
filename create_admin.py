9f858f142bc289a318fbddc118b6d4322f205094
SAkSak2006 <voem37145@gmail.com>
2026-05-19 18:43:50 +0500

    Добавить скрипт создания администратора

---
+++ create_admin.py
@@ -0,0 +1,22 @@
+import asyncio
+from app.database import async_session_maker
+from app.models.user import User
+from passlib.context import CryptContext
+
+pwd = CryptContext(schemes=["bcrypt"])
+
+
+async def create_admin():
+    async with async_session_maker() as session:
+        user = User(
+            username="admin",
+            hashed_password=pwd.hash("admin123"),
+            full_name="Администратор",
+            is_active=True,
+        )
+        session.add(user)
+        await session.commit()
+        print("Администратор создан")
+
+
+asyncio.run(create_admin())
